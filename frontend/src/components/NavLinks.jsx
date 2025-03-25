85e163edde12cd10c682c5db680238a45ce94d7f
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-03-25 18:04:02 -0300

    feat: implementando rotas c/ react router

---
+++ frontend/src/components/NavLinks.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// componente para links reutilizáveis
+
+const NavLinks = () => {
+  return (
+    <>
+      <Link
+        to="/"
+        className="hover:text-verde-paleta hover:shadow duration-500 block"
+      >
+        Home
+      </Link>
+      <Link
+        to="/denuncias"
+        className="hover:text-verde-paleta hover:shadow duration-500 block"
+      >
+        Denúncias
+      </Link>
+      <Link
+        to="/ajuda"
+        className="hover:text-verde-paleta hover:shadow duration-500 block"
+      >
+        Ajuda
+      </Link>
+
+      {/* botôes criar conta e login */}
+      <div className="space-x-2">
+        <Link
+          to="/registro"
+          className="bg-verde-paleta hover:bg-verde-escuro-paleta duration-500 text-azul-paleta font-bold py-2 px-4 rounded inline-flex items-center"
+        >
+          <span className="material-symbols-outlined flex items-center justify-center w-5 h-5 mr-2">
+            person_add
+          </span>
+          Criar Conta
+        </Link>
+        <Link
+          to="/login"
+          className="bg-gray-300 hover:bg-gray-400 duration-500 text-azul-paleta font-bold py-2 px-4 rounded inline-flex items-center"
+        >
+          <span className="material-symbols-outlined flex items-center justify-center w-5 h-5 mr-2">
+            login
+          </span>
+          Entrar
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default NavLinks;
