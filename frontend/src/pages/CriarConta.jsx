3a2bc664a5d176e8ba5397e98f169b32e10eb677
miguelmorandini <miguelmorandini2005@gmail.com>
2025-03-28 18:31:21 -0300

    feat: migrando a pagina "criarconta"

---
+++ frontend/src/pages/CriarConta.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import FormularioCriarConta from '../components/FormularioCriarConta'
+
+const Denuncia = () => {
+  return (
+    <Layout>
+        <FormularioCriarConta />
+    </Layout>
+    
+  )
+}
+
+export default Denuncia
