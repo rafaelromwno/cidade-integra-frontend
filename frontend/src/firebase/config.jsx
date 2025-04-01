c01ddf14b13a9b213d57eb4f1623895825c51ce8
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-04-01 17:56:41 -0300

    feat: implementação do login com firebase

--- frontend/src/firebase/config.jsx
+++ frontend/src/firebase/config.jsx
@@ -2,11 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBgOK-amwIQIOVFlktGsX6pu2LTbeEyL18",
   authDomain: "cidadeunida-3ce06.firebaseapp.com",
@@ -21,5 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
