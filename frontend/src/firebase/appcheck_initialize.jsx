058f70f206623ae3abd27ab79b127b65011e0434
rafaelromwno <romanosilvarafael@gmail.com>
2025-04-09 15:48:28 -0300

    feat: adiciona inicialização do Firebase e configuração do App Check com reCAPTCHA v3

---
+++ frontend/src/firebase/appcheck_initialize.jsx
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
+// configuração do Firebase)
+const firebaseConfig = {
+    apiKey: "AIzaSyBgOK-amwIQIOVFlktGsX6pu2LTbeEyL18",
+    authDomain: "cidadeunida-3ce06.firebaseapp.com",
+    projectId: "cidadeunida-3ce06",
+    storageBucket: "cidadeunida-3ce06.firebasestorage.app",
+    messagingSenderId: "248807744910",
+    appId: "1:248807744910:web:51d7624d93141b340b30df",
+    measurementId: "G-CRVP5ZN46W"
+  };
+
+// inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// inicializa o App Check com o reCAPTCHA v3
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LenGRErAAAAAG8XpxqZKaWvu9YiL-p8lukrrauE"),
+
+  // habilita a atualização automática do token
+  isTokenAutoRefreshEnabled: true,
+});
+
+export default app;
