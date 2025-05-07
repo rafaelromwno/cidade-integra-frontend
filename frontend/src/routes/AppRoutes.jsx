2d42d68a5371ff444497c36f0cc51bbf83d1a27e
rafaelromwno <romanosilvarafael@gmail.com>
2025-05-07 16:04:41 -0300

    clean: removendo a 'dist' do rastreamento

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,33 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CriarConta from "../pages/CriarConta";
-import Denuncia from "../pages/Denuncia";
-import Duvidas from "../pages/Duvidas";
-import Entrar from "../pages/Entrar";
-import Paginainicial from "../pages/PaginaInicial";
-import NotFound from "../pages/NotFound";
-import EsqueciSenha from "../pages/EsqueciSenha";
-import Perfil from "../pages/Perfil";
-import ProtectedRoute from "../routes/ProtectedRoute";
+
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Paginainicial />} />
-        <Route path="/criar-conta" element={<CriarConta />} />
-        <Route path="/denuncie" element={<Denuncia />} />
-        <Route path="/duvidas" element={<Duvidas />} />
-        <Route path="/entrar" element={<Entrar />} />
-        <Route path="/esqueci-senha" element={<EsqueciSenha />} />
-        <Route
-          path="/perfil"
-          element={
-            <ProtectedRoute>
-              <Perfil />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
+       
       </Routes>
     </BrowserRouter>
   );
