22a7bfede2019916decb4c9331dd4bec34017513
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-05-08 17:47:30 -0300

    fix: corrigindo menu 'flutuante'

--- frontend/src/components/layout/DesktopMenu.jsx
+++ frontend/src/components/layout/DesktopMenu.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { Home, Bell, User, LogIn, Shield, LogOut, Plus } from 'lucide-react';
 
-const DesktopMenu = ({ user, onLogout }) => {
+const DesktopMenu = ({ user, onLogout, isLoggingOut }) => {
   return (
     <>
       <Link to="/" className="hover:text-verde transition-colors flex items-center gap-1">
         <Home size={18} />
         <span>Início</span>
       </Link>
-      
+
       <Link to="/denuncias" className="hover:text-verde transition-colors flex items-center gap-1">
         <Bell size={18} />
         <span>Denúncias</span>
@@ -29,7 +29,6 @@ const DesktopMenu = ({ user, onLogout }) => {
 
       {user && (
         <>
-
           <Link to="/perfil" className="hover:text-verde transition-colors flex items-center gap-1">
             <User size={18} />
             <span>Perfil</span>
@@ -39,27 +38,27 @@ const DesktopMenu = ({ user, onLogout }) => {
             <Shield size={18} />
             <span>Admin</span>
           </Link>
-
         </>
       )}
 
-<Button asChild className="bg-verde hover:bg-verde-escuro text-azul font-semibold duration-500 ">
-                <Link to="/nova-denuncia" className="flex items-center gap-1">
-                    <Plus className="h-4 w-4" />
-                    <span>Nova Denúncia</span>
-                </Link>
-            </Button>
+      <Button asChild className="bg-verde hover:bg-verde-escuro text-azul font-semibold duration-500">
+        <Link to="/nova-denuncia" className="flex items-center gap-1">
+          <Plus className="h-4 w-4" />
+          <span>Nova Denúncia</span>
+        </Link>
+      </Button>
 
-            {user && (
-                <Button
-                    onClick={onLogout}
-                    className="hover:text-white text-white flex items-center gap-1"
-                    variant="destructive"
-                >
-                    <LogOut size={18} />
-                    <span>Sair</span>
-                </Button>
-            )}
+      {user && (
+        <Button
+          onClick={onLogout}
+          disabled={isLoggingOut}
+          className="hover:text-white text-white flex items-center gap-1"
+          variant="destructive"
+        >
+          <LogOut size={18} />
+          <span>{isLoggingOut ? "Saindo..." : "Sair"}</span>
+        </Button>
+      )}
     </>
   );
 };
