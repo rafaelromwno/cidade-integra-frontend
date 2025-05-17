20b91e92569cc0e4bb23b5b0b9ff2a64baa80b6e
Rafael Romano Silva <romanosilvarafael@gmail.com>
2025-05-17 14:06:21 -0300

    style: aumentando visibilidade da mensagem 'carregando'

--- frontend/src/components/ui/LoadingScreen.jsx
+++ frontend/src/components/ui/LoadingScreen.jsx
@@ -3,7 +3,7 @@ const LoadingScreen = ({ mensagem = "Carregando..." }) => (
     <main className="flex-grow flex justify-center items-center">
       <div className="flex flex-col items-center">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-verde"></div>
-        <span className="mt-4 text-verde text-lg">{mensagem}</span>
+        <span className="mt-4 text-verde text-lg font-semibold">{mensagem}</span>
       </div>
     </main>
   </div>
